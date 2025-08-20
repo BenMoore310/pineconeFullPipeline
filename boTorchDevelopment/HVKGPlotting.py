d52904a33c1b6caaf7e240d8b5cdad89a561dca8
Ben Moore <benjamin.moore00@gmail.com>
2025-08-20 13:39:32 +0100

    generalised plotting script

--- boTorchDevelopment/HVKGPlotting.py
+++ boTorchDevelopment/HVKGPlotting.py
@@ -6,56 +6,71 @@ from pymoo.problems import get_problem
 from pymoo.util.plotting import plot
 from pymoo.util.ref_dirs import get_reference_directions
 from pymoo.visualization.scatter import Scatter
+import argparse
 
-ref_dirs = get_reference_directions("das-dennis", 2, n_partitions=20)
-pf = get_problem("dtlz2").pareto_front(ref_dirs)
+def main(function):
 
-# save the pareto front as a numpy array and plot it
-pfArray = np.array(pf)
+    ref_dirs = get_reference_directions("das-dennis", 2, n_partitions=20)
+    pf = get_problem(function).pareto_front(ref_dirs)
 
-# read in each iteration's features, targets, and std
+    # save the pareto front as a numpy array and plot it
+    pfArray = np.array(pf)
 
-filesList = glob.glob("modelParetoFronts/features/*")
+    # read in each iteration's features, targets, and std
+
+    filesList = glob.glob("modelParetoFronts/features/*")
 
 
-for iteration in range(len(filesList)):
-    features = np.loadtxt(f"modelParetoFronts/features/featuresIter{iteration}.txt")
-    targets = np.loadtxt(f"modelParetoFronts/targets/targetsIter{iteration}.txt")
-    std = np.loadtxt(f"modelParetoFronts/uncertainties/stdIter{iteration}.txt")
+    for iteration in range(len(filesList)):
+        features = np.loadtxt(f"modelParetoFronts/features/featuresIter{iteration}.txt")
+        targets = np.loadtxt(f"modelParetoFronts/targets/targetsIter{iteration}.txt")
+        std = np.loadtxt(f"modelParetoFronts/uncertainties/stdIter{iteration}.txt")
 
-    # plot targets with error bars
-    plt.figure(figsize=(10, 10))
-    plt.errorbar(
-        targets[:, 0],
-        targets[:, 1],
-        xerr=std[:, 0],
-        yerr=std[:, 1],
-        fmt="o",
-        label="Objective 1",
-        capsize=5,
-        errorevery=1,
+        # plot targets with error bars
+        plt.figure(figsize=(10, 10))
+        plt.errorbar(
+            targets[:, 0],
+            targets[:, 1],
+            xerr=std[:, 0],
+            yerr=std[:, 1],
+            fmt="o",
+            label="Objective 1",
+            capsize=5,
+            errorevery=1,
+        )
+        plt.scatter(
+            pfArray[:, 0], pfArray[:, 1], c="black", marker="x", label="Pareto Front"
+        )
+        plt.title(f"Iteration {iteration} - Targets with Uncertainty")
+        plt.xlabel("Objective 1")
+        plt.ylabel("Objective 2")
+        # plt.xlim(-0.2, 1.5)
+        # plt.ylim(-0.2, 1.5)
+        # plt.grid(True)
+        plt.savefig(f"modelParetoFronts/paretoPlots/iteration_{iteration}.png")
+        plt.close()
+        # plt.show()
+
+    # load in all saved figures and generate an animated GIF
+    import imageio
+
+    images = []
+    for iteration in range(len(filesList)):
+        images.append(
+            imageio.imread(f"modelParetoFronts/paretoPlots/iteration_{iteration}.png")
+        )
+    imageio.mimsave("pareto_animation.gif", images, duration=1)
+    # print completion message
+    print("Plots generated and saved as GIF.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate Pareto Front Plots")
+    parser.add_argument(
+        "--function",
+        type=str,
+        default="dtlz2",
+        help="The optimization problem to visualize (default: zdt1)",
     )
-    plt.scatter(
-        pfArray[:, 0], pfArray[:, 1], c="black", marker="x", label="Pareto Front"
-    )
-    plt.title(f"Iteration {iteration} - Targets with Uncertainty")
-    plt.xlabel("Objective 1")
-    plt.ylabel("Objective 2")
-    plt.xlim(-0.2, 1.5)
-    plt.ylim(-0.2, 1.5)
-    # plt.grid(True)
-    plt.savefig(f"modelParetoFronts/paretoPlots/iteration_{iteration}.png")
-    plt.close()
-    # plt.show()
-
-# load in all saved figures and generate an animated GIF
-import imageio
-
-images = []
-for iteration in range(len(filesList)):
-    images.append(
-        imageio.imread(f"modelParetoFronts/paretoPlots/iteration_{iteration}.png")
-    )
-imageio.mimsave("pareto_animation.gif", images, duration=1)
-# print completion message
-print("Plots generated and saved as GIF.")
+    args = parser.parse_args()
+    main(args.function)
